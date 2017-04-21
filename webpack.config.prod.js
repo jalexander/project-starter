78f06f2aa42057819dcf59127cc92e0dedfce81b
Jim Alexander <jim@jralexander.com>
2017-04-21 14:19:41 -0400

    adding query string for activating mock api

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  devtool: 'source-map',
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    modules: [ 'node_modules' ],
+  }
+}
