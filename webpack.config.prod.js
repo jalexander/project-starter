10455ad0774468fa5acae5af04f407c9f5558c26
Jim Alexander <jim@jralexander.com>
2017-04-22 18:43:34 -0400

    adding an inline chunk manifest for long term caching

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,8 @@ import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import ChunkManifestPlugin from 'chunk-manifest-webpack-plugin';
+import InlineManifestWebpackPlugin from 'inline-manifest-webpack-plugin';
 
 const devtool = 'source-map'
 const isSourceMap = devtool.indexOf('sourcemap') >= 0 || devtool.indexOf('source-map') >= 0
@@ -16,18 +18,25 @@ export default {
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
-    filename: '[name].[chunkhash].js'
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
   },
   plugins: [
     // generate an external css file with a hash in the filename
     new ExtractTextPlugin('[name].[contenthash].css'),
 
+    new webpack.optimize.CommonsChunkPlugin({
+      // vendor libs + extracted manifest
+      name: ['vendor', 'manifest'],
+      minChunks: Infinity,
+    }),
+
+    new webpack.HashedModuleIdsPlugin(),
     // hash the files using MD5 so that their names change when the content changes
     new WebpackMd5Hash(),
-
-    // create a separate bundle of vendor libraries
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
+    new ChunkManifestPlugin({
+      filename: 'chunk-manifest.json',
+      manifestVariable: 'webpackManifest'
     }),
 
     // create HTML file that includes reference to bundled js
@@ -46,6 +55,10 @@ export default {
         minifyURLs: true
       },
       inject: true
+    }),
+
+    new InlineManifestWebpackPlugin({
+      name: 'webpackManifest'
     }),
 
     // minify js
